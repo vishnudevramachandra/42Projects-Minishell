6e253481f524e873cd8e29f347fe8a99b6039f2f
Simon Wied <swied@1-E-13.42heilbronn.de>
2025-07-17 18:00:04 +0200

    ADD(execution): Add get_path.c for getting the path for cmd execution | Add builtin.c for managing which builtin getting executed

---
+++ src/builtin.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtin.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: swied <swied@student.42heilbronn.de>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/17 15:53:58 by swied             #+#    #+#             */
+/*   Updated: 2025/07/17 16:50:12 by swied            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/execute.h"
+
+int	execute_builtin(t_cmd_node *cmd_node, char **envp)
+{
+	if (ft_strcmp(cmd_node->cmd[0], "cd") == 0)
+		return (builtin_cd(cmd_node->cmd, &envp), 0);
+	else if (ft_strcmp(cmd_node->cmd[0], "pwd") == 0)
+		return (builtin_pwd(), 0);
+	else if (ft_strcmp(cmd_node->cmd[0], "echo") == 0)
+		return (builtin_echo(cmd_node->cmd), 0);
+	return (1);
+}
