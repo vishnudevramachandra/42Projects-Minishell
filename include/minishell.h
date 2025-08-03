7c9dfa1983cb46ee677832eaad5a58b8bb9baa9f
Simon Wied <swied@1-E-29.42heilbronn.de>
2025-08-03 15:34:25 +0200

    FEAT(execution): Added hd_list and hd_node to minishell.h

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: swied <swied@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/16 18:42:38 by swied             #+#    #+#             */
-/*   Updated: 2025/07/29 02:33:53 by swied            ###   ########.fr       */
+/*   Updated: 2025/08/03 15:30:38 by swied            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,25 +18,26 @@ typedef struct s_file_node t_file_node;
 typedef struct s_cmd_node t_cmd_node;
 typedef	struct s_hd_list t_hd_list;
 typedef struct s_hd_node t_hd_node;
+typedef struct s_hd_line t_hd_line;
 
 typedef struct s_hd_line
 {
-	char				*lines;
-	struct t_hd_line	*next;
+	char				*line;
+	t_hd_line			*next;
 }	t_hd_line;
 
 typedef struct s_hd_node
 {
-	t_file_node	*file_node;
+	t_file_node			*file_node;
 	t_hd_line			*lines;
 	char				*lim;
-	t_hd_node	*next;
+	t_hd_node			*next;
 }	t_hd_node;
 
 typedef struct s_hd_list
 {
-	t_hd_node	*head;	
-	t_hd_node	*tail;
+	t_hd_node			*head;	
+	t_hd_node			*tail;
 	size_t				size;
 }	t_hd_list;
 
@@ -50,41 +51,41 @@ typedef enum e_redir_type
 
 typedef struct s_file_node
 {
-	char			*filename;
-	t_redir_type	redir_type;
-	t_file_node		*next;
+	char				*filename;
+	t_redir_type		redir_type;
+	t_file_node			*next;
 }	t_file_node;
 
 typedef struct s_file_list
 {
-	t_file_node	*head;
-	t_file_node	*tail;	
-	ssize_t		size;
-	int			fd_infile;
-	int			fd_outfile;
+	t_file_node		*head;
+	t_file_node		*tail;	
+	ssize_t			size;
+	int				fd_infile;
+	int				fd_outfile;
 }	t_file_list;
 
 typedef struct s_cmd_node
 {
-	int			cmd_type;
-	char		**cmd;
-	t_file_list	*file;
-	t_cmd_node	*next;
-	t_hd_list	*hd_list;
+	int				cmd_type;
+	char			**cmd;
+	t_file_list		*file;
+	t_cmd_node		*next;
+	t_hd_list		*hd_list;
 }	t_cmd_node;
 
 typedef struct s_cmd_list
 {
-	t_cmd_node	*head;
-	t_cmd_node	*tail;
-	ssize_t		size;
-	int			prev_fd;
+	t_cmd_node		*head;
+	t_cmd_node		*tail;
+	ssize_t			size;
+	int				prev_fd;
 	
 }	t_cmd_list;
 
 typedef struct s_mini
 {
-	t_cmd_list	*cmd_list;
+	t_cmd_list		*cmd_list;
 }	t_mini;
 
 #endif
