2b46fc4d1e3d7d1feab673f3c2682ad550439388
Simon Wied <swied@1-A-26.42heilbronn.de>
2025-08-04 17:56:38 +0200

    FIX(execution): Fixed so only is_export args will get printed with export as cmd

--- src/export.c
+++ src/export.c
@@ -6,7 +6,7 @@
 /*   By: swied <swied@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/04 16:45:00 by swied             #+#    #+#             */
-/*   Updated: 2025/08/04 17:49:44 by swied            ###   ########.fr       */
+/*   Updated: 2025/08/04 17:55:53 by swied            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,12 @@ int	builtin_export(t_env_list *env_list)
 	current = env_list->head;
 	while (current)
 	{
-		ft_putstr_fd(current->variable, 1);
-		ft_putchar_fd('=', 1);
-		ft_putendl_fd(current->value, 1);
+		if (current->is_export)
+		{
+			ft_putstr_fd(current->variable, 1);
+			ft_putchar_fd('=', 1);
+			ft_putendl_fd(current->value, 1);
+		}
 		current = current->next;
 	}
 	return (0);
