1c16435f646ffc50d6b5fc8a301fd71f6b964ec3
Simon Wied <swied@2-F-4.42heilbronn.de>
2025-08-05 18:50:27 +0200

    ADD(execution): Added list_to_dblarray.c to give the env_list as doublearray to execve

--- src/execute.c
+++ src/execute.c
@@ -6,20 +6,21 @@
 /*   By: swied <swied@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/14 23:04:36 by swied             #+#    #+#             */
-/*   Updated: 2025/08/04 19:04:11 by swied            ###   ########.fr       */
+/*   Updated: 2025/08/05 18:20:30 by swied            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/execute.h"
 
-int	execute_cmd_or_builtin(t_cmd_node *cmd_node, char **envp, t_env_list *env_list)
+int	execute_cmd_or_builtin(t_cmd_node *cmd_node, char **envp,
+	t_env_list *env_list)
 {
 	int	status;
 
 	status = 0;
 	if (cmd_node->cmd_type == 1)
 	{
-		status = execute_builtin(cmd_node, envp, env_list);
+		status = execute_builtin(cmd_node, env_list);
 		exit(status);
 	}
 	else
@@ -36,7 +37,7 @@ int	execute_loop(t_cmd_list *cmd_list, char **envp, t_env_list *env_list)
 
 	if (cmd_list->size == 1 && cmd_list->head->cmd_type == 1)
 	{
-		status = execute_builtin(cmd_list->head, envp, env_list);
+		status = execute_builtin(cmd_list->head, env_list);
 		return (status);
 	}
 	execute_pipes(cmd_list, envp, env_list);
