74acd28c1669c3dcc40069e1c9ffddd0ee543054
Simon Wied <swied@2-F-4.42heilbronn.de>
2025-08-05 18:22:06 +0200

    FIX(execution): Changed use of envp to use of env_list in cd

--- src/cd.c
+++ src/cd.c
@@ -6,43 +6,35 @@
 /*   By: swied <swied@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/15 02:17:26 by swied             #+#    #+#             */
-/*   Updated: 2025/07/16 14:51:35 by swied            ###   ########.fr       */
+/*   Updated: 2025/08/05 18:20:01 by swied            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/execute.h"
 
-int	builtin_cd(char **args, char ***env)
+int	builtin_cd(char **args, t_env_list *env_list)
 {
-	char *target_dir;
-	char *current_pwd;
+	char	*target_dir;
+	char	*old_pwd;
 	char	*new_pwd;
-	
-	(void)env;
-	current_pwd = getcwd(NULL, 0);
-	if (!current_pwd)
-	{
-		perror("minishell: cd: getcwd");
-		return (1);
-	}
+
 	if (!args[1])
-		target_dir = ft_strdup(getenv("HOME"));
-	else if (strcmp(args[1], "-") == 0)
-		target_dir = ft_strdup(getenv("OLDPWD"));
+		target_dir = get_env_value(env_list, "HOME");
+	else if (ft_strcmp(args[1], "-") == 0)
+		target_dir = get_env_value(env_list, "OLDPWD");
 	else
-		target_dir = ft_strdup(args[1]);
-	if (!target_dir)
-		return (ft_putstr_fd("HOME not set\n", 1), free(current_pwd), 1);
-	if (chdir(target_dir) == -1)
-		return (ft_putstr_fd("Directory not found", 2), free(current_pwd), free(target_dir), 1);
-	update_env_var(env, "OLDPWD", current_pwd);
-	new_pwd = getcwd(NULL, 0);
-	if (new_pwd)
+		target_dir = args[1];
+	old_pwd = getcwd(NULL, 0);
+	add_to_gc(old_pwd);
+	if (chdir(target_dir))
 	{
-		update_env_var(env, "PWD", new_pwd);
-		free(new_pwd);
+		ft_putstr_fd("minishell: no such file or directory: ", 2);
+		return (ft_putendl_fd(target_dir, 2), 1);
 	}
-	free(current_pwd);
-	free(target_dir);
-	return (0);
+	set_env_var(env_list, "OLDPWD", old_pwd);
+	new_pwd = getcwd(NULL, 0);
+	if (!new_pwd)
+		return (ft_putendl_fd("minishell: cd failed", 2), 1);
+	set_env_var(env_list, "PWD", new_pwd);
+	return (add_to_gc(new_pwd), 0);
 }
