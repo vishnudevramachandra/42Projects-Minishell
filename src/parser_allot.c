28c812eae47a66cd829097e32a786782a8e716ac
vishnudev ramachandra <vishnudevramachandra@gmail.com>
2025-09-05 13:55:28 +0200

    fix: missing files added

---
+++ src/parser_allot.c
@@ -0,0 +1,92 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_allot.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vishnudevramachandra <vishnudevramachan    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/05 01:14:48 by vishnudevra       #+#    #+#             */
+/*   Updated: 2025/09/05 12:59:49 by vishnudevra      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/parser.h"
+
+static t_file_node	*create_file_node(
+	char *name, t_redir_type typ, t_lexer *lex)
+{
+	t_file_node	*file;
+
+	file = gc_malloc(sizeof(t_file_node));
+	if (!file)
+		free_and_exit(lex);
+	if (!name)
+		file->filename = NULL;
+	else
+	{
+		file->filename = gc_malloc(ft_strlen(name) + 1);
+		if (!file->filename)
+			free_and_exit(lex);
+		ft_strlcpy(file->filename, name, ft_strlen(name) + 1);
+	}
+	file->redir_type = typ;
+	file->next = NULL;
+	return (file);
+}
+
+/* setup file node, allot filename and append it to file list */
+void	add_file_to_filelist(
+	t_file_list *files, char *name, t_redir_type typ, t_lexer *lex)
+{
+	t_file_node	*file;
+	t_file_node	*node;
+
+	file = create_file_node(name, typ, lex);
+	if (files->size < 1)
+		files->head = file;
+	else
+	{
+		node = files->head;
+		while (node->next)
+			node = node->next;
+		node->next = file;
+	}
+	files->tail = file;
+	files->size++;
+}
+
+static char	*str_dup(char *src, size_t len, t_lexer *lex)
+{
+	char	*dst;
+
+	dst = gc_malloc(len + 1);
+	if (!dst)
+		free_and_exit(lex);
+	ft_strlcpy(dst, src, len + 1);
+	return (dst);
+}
+
+/* appends the argument "word" to command array (of strings) */
+char	**update_cmd(char **cmd, char *word, t_lexer *lex)
+{
+	char	**strs;
+	size_t	len;
+	size_t	i;
+
+	len = 0;
+	if (cmd)
+		while (cmd[len])
+			len++;
+	strs = gc_malloc((len + 2) * sizeof(char *));
+	if (!strs)
+		free_and_exit(lex);
+	i = 0;
+	while ((i + 1) < len)
+	{
+		strs[i] = str_dup(cmd[i], ft_strlen(cmd[i]), lex);
+		i++;
+	}
+	strs[i] = str_dup(word, ft_strlen(word), lex);
+	strs[len] = 0;
+	return (strs);
+}
---
+++ src/parser_inits.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser_inits.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vishnudevramachandra <vishnudevramachan    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/04 21:26:11 by vishnudevra       #+#    #+#             */
+/*   Updated: 2025/09/05 13:09:57 by vishnudevra      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/parser.h"
+
+void	free_and_exit(t_lexer *lex)
+{
+	clear_lexer(lex);
+	free_all_garbage();
+	exit(1);
+}
+
+t_cmd_node	*alloc_and_init_cmd_node(t_lexer *lex)
+{
+	t_cmd_node	*cmd_node;
+
+	cmd_node = gc_malloc(sizeof(t_cmd_node));
+	if (!cmd_node)
+	free_and_exit(lex);
+	cmd_node->cmd_type = 0;
+	cmd_node->cmd = NULL;
+	cmd_node->file_list = NULL;
+	cmd_node->next = NULL;
+	cmd_node->hd_list = NULL;
+	return (cmd_node);
+}
+
+t_file_list	*alloc_and_init_file_list(t_lexer *lex)
+{
+	t_file_list	*files;
+
+	files = gc_malloc(sizeof(t_file_list));
+	if (!files)
+		free_and_exit(lex);
+	files->head = NULL;
+	files->tail = NULL;
+	files->size = 0;
+	files->fd_infile = -1;
+	files->fd_outfile = -1;
+	return (files);
+}
+
+void	init_cmds(t_cmd_list *cmds, t_lexer *lex)
+{
+	if (lex->toks)
+	{
+		cmds->head = alloc_and_init_cmd_node(lex);
+		cmds->tail = cmds->head;
+		cmds->size = 1;
+	}
+	else
+	{
+		cmds->head = NULL;
+		cmds->tail = NULL;
+		cmds->size = 0;
+	}
+	cmds->prev_fd = -1;
+}
