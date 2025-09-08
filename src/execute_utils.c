87be64e8418360f1e093e22927c65abfd15178ab
AimonKied <simon.wied2000@gmail.com>
2025-09-08 15:39:11 +0200

    FIX(utils): Updated ft_strcmp to more stable version

--- src/execute_utils.c
+++ src/execute_utils.c
@@ -6,7 +6,7 @@
 /*   By: swied <swied@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/14 23:28:15 by swied             #+#    #+#             */
-/*   Updated: 2025/08/06 19:24:09 by swied            ###   ########.fr       */
+/*   Updated: 2025/09/08 15:32:45 by swied            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,10 @@
 /* Basic string compare */
 int	ft_strcmp(const char *s1, const char *s2)
 {
-	size_t	i;
-
-	i = 1;
-	while (s1[i])
+	while (*s1 && *s2 && *s1 == *s2)
 	{
-		if (*s1 == '\0' || *s2 == '\0')
-			return ((unsigned char)*s1 - (unsigned char)*s2);
-		else if (*s1 == *s2)
-		{
-			s1++;
-			s2++;
-		}
-		else
-			return ((unsigned char)*s1 - (unsigned char)*s2);
-		i++;
+		s1++;
+		s2++;
 	}
 	return ((unsigned char)*s1 - (unsigned char)*s2);
 }
