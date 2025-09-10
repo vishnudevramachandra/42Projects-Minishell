98922330b9e2b269e6e05c8c3c56cd86448b2301
AimonKied <simon.wied2000@gmail.com>
2025-09-10 17:27:08 +0200

    ADD(heredoc): Add heredoc_output.c with all outputing and from user collecting functions

---
+++ src/heredoc_output.c
@@ -0,0 +1,106 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   heredoc_output.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: swied <swied@student.42heilbronn.de>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/10 17:17:00 by swied             #+#    #+#             */
+/*   Updated: 2025/09/10 17:18:37 by swied            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/execute.h"
+
+/* Print EOF warning message */
+static void	print_eof_warning(char *delimiter)
+{
+	write(STDERR_FILENO, "minishell: warning: here-document delimited by end-of-file (wanted `", 67);
+	write(STDERR_FILENO, delimiter, ft_strlen(delimiter));
+	write(STDERR_FILENO, "')\n", 3);
+}
+
+/* Collect heredoc input with proper signal handling */
+int	collect_heredoc_input(char *delimiter, int write_fd)
+{
+	char	*line;
+
+	setup_heredoc_signals();
+	while (1)
+	{
+		write(STDERR_FILENO, "> ", 2);
+		line = get_next_line(STDIN_FILENO);
+		if (!line)
+		{
+			print_eof_warning(delimiter);
+			break ;
+		}
+		if (is_delimiter_match(line, delimiter))
+		{
+			free(line);
+			break ;
+		}
+		write(write_fd, line, ft_strlen(line));
+		free(line);
+	}
+	restore_default_signals();
+	close(write_fd);
+	exit(get_heredoc_interrupted() ? 130 : 0);
+}
+
+/* Read heredoc content from pipe and store in lines */
+int	read_heredoc_content(t_hd_node *hd_node, int read_fd)
+{
+	char	*line;
+	char	*stored_line;
+
+	while (1)
+	{
+		line = get_next_line(read_fd);
+		if (!line)
+			break ;
+		stored_line = ft_strdup(line);
+		if (!stored_line)
+		{
+			free(line);
+			return (-1);
+		}
+		add_to_gc(stored_line);
+		add_line_to_heredoc(hd_node, stored_line);
+		free(line);
+	}
+	return (0);
+}
+
+/* Write heredoc content to pipe */
+int	write_heredoc_to_pipe(t_hd_node *hd_node, int write_fd)
+{
+	t_hd_line	*current;
+
+	if (!hd_node || !hd_node->lines)
+		return (-1);
+	current = hd_node->lines;
+	while (current)
+	{
+		if (current->line)
+		{
+			write(write_fd, current->line, ft_strlen(current->line));
+		}
+		current = current->next;
+	}
+	return (0);
+}
+
+/* Create pipe and write heredoc content to it */
+int	create_heredoc_fd(t_hd_node *hd_node)
+{
+	int	pipe_fd[2];
+
+	if (!hd_node)
+		return (-1);
+	if (pipe(pipe_fd) == -1)
+		return (-1);
+	write_heredoc_to_pipe(hd_node, pipe_fd[1]);
+	close(pipe_fd[1]);
+	return (pipe_fd[0]);
+}
