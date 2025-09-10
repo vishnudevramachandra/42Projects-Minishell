849a49e491c0f3d6e730f4e73161b5b491f0e8ed
AimonKied <simon.wied2000@gmail.com>
2025-09-10 15:00:19 +0200

    FIX(execute): fixed issue with handling export without value and export without equalsign

--- src/export_add.c
+++ src/export_add.c
@@ -6,7 +6,7 @@
 /*   By: swied <swied@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/24 04:09:11 by swied             #+#    #+#             */
-/*   Updated: 2025/09/09 15:21:04 by swied            ###   ########.fr       */
+/*   Updated: 2025/09/10 14:59:23 by swied            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,19 +27,15 @@ int add_export(t_env_list *env_list, t_cmd_node *cmd_node)
 int process_export_arg(t_env_list *env_list, char *arg)
 {
 	int var_len;
-	int val_len;
 	char *var;
 
 	var_len = 0;
-	val_len = 0;
 	if (!search_for_equal_sign(arg))
 		return (handle_export_without_equal(env_list, arg));
 	while (arg[var_len] && arg[var_len] != '=')
 		var_len++;
-	while (arg[var_len + val_len] && arg[var_len + val_len] != ' ')
-		val_len++;
 	var = ft_substr(arg, 0, var_len);
-	if (val_len == 0)
+	if (arg[var_len + 1] == '\0')
 	{
 		if (handle_export_without_value(env_list, var) == -1)
 			return (free(var), -1);
@@ -55,23 +51,22 @@ int process_export_arg(t_env_list *env_list, char *arg)
 
 int	handle_export_without_value(t_env_list *env_list, char *var)
 {
-	t_env_node *current;
+	t_env_node *existing;
 
-	current = env_list->head;
-	while (current)
+	existing = find_env_var(env_list, var);
+	if (existing)
 	{
-		if (ft_strcmp(var, current->variable) == 0)
-		{
-			unset_env_var(env_list, var);
-			break;
-		}
-		current = current->next;
+		if (existing->value)
+			gc_free(existing->value);
+		existing->value = ft_strdup("");
+		if (!existing->value)
+			return (-1);
+		add_to_gc(existing->value);
+		existing->is_export = 1;
+		return (0);
 	}
 	if (!add_env_var(env_list, var, ""))
-	{
-		free(var);
 		return (-1);
-	}
 	return (0);
 }
 
@@ -88,28 +83,16 @@ int	search_for_equal_sign(char *arg)
 
 int	handle_export_without_equal(t_env_list *env_list, char *arg)
 {
-	t_env_node *new_node;
-	t_env_node *last;
+	t_env_node *existing;
 
-	new_node = gc_malloc(sizeof(t_env_node));
-	if (!new_node)
-		return (-1);
-	new_node->variable = ft_strdup(arg);
-	new_node->value = NULL;
-	new_node->next = NULL;
-	new_node->is_export = false;
-	env_list->size++;
-	if (!new_node->variable)
-		return (free(new_node), -1);
-	add_to_gc(new_node->variable);
-	if (!env_list->head)
-		env_list->head = new_node;
-	else
+	existing = find_env_var(env_list, arg);
+	if (existing)
 	{
-		last = env_list->head;
-		while (last->next)
-			last = last->next;
-		last->next = new_node;
+		existing->is_export = 1;
+		return (0);
 	}
+	// Create new variable without value (export VAR) - no value at all
+	if (!add_env_var(env_list, arg, NULL))
+		return (-1);
 	return (0);
 }
