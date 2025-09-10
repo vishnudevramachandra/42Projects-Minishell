d22d877adc99174f0eef68434e425b7d01ec9f0a
AimonKied <simon.wied2000@gmail.com>
2025-09-10 17:28:22 +0200

    ADD(heredoc): Add heredoc_signals.c with all heredoc signal functions

---
+++ src/heredoc_signals.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   heredoc_signals.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: swied <swied@student.42heilbronn.de>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/10 16:00:00 by swied             #+#    #+#             */
+/*   Updated: 2025/09/10 15:50:16 by swied            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/execute.h"
+
+static volatile sig_atomic_t g_heredoc_interrupted = 0;
+
+/* Signal handler for heredoc input */
+void	heredoc_signal_handler(int signum)
+{
+	if (signum == SIGINT)
+	{
+		g_heredoc_interrupted = 1;
+		write(STDERR_FILENO, "\n", 1);
+		exit(130);
+	}
+}
+
+/* Setup signal handling for heredoc */
+void	setup_heredoc_signals(void)
+{
+	struct sigaction	sa;
+
+	sa.sa_handler = heredoc_signal_handler;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_RESTART;
+	sigaction(SIGINT, &sa, NULL);
+	signal(SIGQUIT, SIG_IGN);
+}
+
+/* Restore default signal handling */
+void	restore_default_signals(void)
+{
+	signal(SIGINT, SIG_DFL);
+	signal(SIGQUIT, SIG_DFL);
+}
+
+/* Get heredoc interrupt status */
+int	get_heredoc_interrupted(void)
+{
+	return (g_heredoc_interrupted);
+}
+
+/* Reset heredoc interrupt status */
+void	reset_heredoc_interrupted(void)
+{
+	g_heredoc_interrupted = 0;
+}
