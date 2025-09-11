9a28a620369a401705503f3eb1e8b28ed1c2a858
Simon Wied <swied@3-B-11.42heilbronn.de>
2025-09-11 12:51:24 +0200

    FIX(execute): Add close(fd) to reset_stds so no invalid fd closing in valgrind anymore

--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,7 @@
 /*   By: swied <swied@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/09 16:34:02 by swied             #+#    #+#             */
-/*   Updated: 2025/09/09 16:42:54 by swied            ###   ########.fr       */
+/*   Updated: 2025/09/11 12:41:02 by swied            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,19 +26,22 @@ int	reset_stds(void)
 	int	i;
 	int	fd;
 
-	i = -1;
+	i = 3;
 	while (i < 1023)
 	{
-		if (check_if_fd_open(i) == -1)
+		if (check_if_fd_open(i) == 0)
 			close(i);
 		i++;
 	}
 	fd = open("/dev/tty", O_RDONLY);
 	dup2(fd, STDIN_FILENO);
+	close(fd);
 	fd = open("/dev/tty", O_WRONLY);
 	dup2(fd, STDOUT_FILENO);
+	close(fd);
 	fd = open("/dev/tty", O_WRONLY);
 	dup2(fd, STDERR_FILENO);
+	close(fd);
 	return (-1);
 }
 
