cc98ce050333028e80f9fc909adbf997a8db7a2c
Simon Wied <swied@2-B-9.42heilbronn.de>
2025-09-20 05:34:12 +0200

    FIX(builtin): added check if exit argument is not a number -> exit status 2

--- src/exit.c
+++ src/exit.c
@@ -6,11 +6,25 @@
 /*   By: swied <swied@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/03 10:58:06 by swied             #+#    #+#             */
-/*   Updated: 2025/09/20 02:31:20 by swied            ###   ########.fr       */
+/*   Updated: 2025/09/20 05:32:45 by swied            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/execute.h"
+
+static int	is_numstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] < 48 || str[i] > 57)
+			return (0);
+		i++;
+	}
+	return (1);
+}
 
 /* not done yet */
 int	builtin_exit(char **args)
@@ -21,6 +35,12 @@ int	builtin_exit(char **args)
 	ft_putendl_fd("exit", 1);
 	if (args && args[1])
 	{
+		if (!is_numstr((args[1])))
+		{
+			free_all_garbage();
+			rl_clear_history();
+			exit(2);
+		}
 		exit_code = ft_atoi(args[1]);
 		if (args[2])
 		{
