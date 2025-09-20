c751bd8ca1c6a8fbdc0a17fe36c1b2253e06e682
Simon Wied <swied@2-B-9.42heilbronn.de>
2025-09-20 02:40:34 +0200

    FIX(parse): changed the name allocation to manually malloc from gc_mallo and free it in function because no use afterwards

--- src/lexer_exp.c
+++ src/lexer_exp.c
@@ -6,7 +6,7 @@
 /*   By: swied <swied@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/27 21:23:43 by vishnudevra       #+#    #+#             */
-/*   Updated: 2025/09/09 14:40:35 by swied            ###   ########.fr       */
+/*   Updated: 2025/09/20 02:23:36 by swied            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,12 @@ static size_t	extract_val_of_var(
 
 	len = ft_strspn(linebuffer,
 			"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_");
-	name = gc_malloc((len + 1) * sizeof(char));
+	name = malloc((len + 1) * sizeof(char));
 	if (!name)
 		return (0);
 	ft_strlcpy(name, linebuffer, len + 1);
 	*val = get_env_value(env_list, name);
+	free(name);
 	return (len);
 }
 
