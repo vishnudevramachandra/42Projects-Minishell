505d30a813cc1999f49a6cda13c6f05201918fa1
Simon Wied <swied@2-B-9.42heilbronn.de>
2025-09-20 05:42:20 +0200

    FIX(builtin): updated order of checks for numeric input and more then one argument with exit

--- src/exit.c
+++ src/exit.c
@@ -6,7 +6,7 @@
 /*   By: swied <swied@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/03 10:58:06 by swied             #+#    #+#             */
-/*   Updated: 2025/09/20 05:32:45 by swied            ###   ########.fr       */
+/*   Updated: 2025/09/20 05:41:19 by swied            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,16 @@ static int	is_numstr(char *str)
 {
 	int i;
 
+	if (!str || !*str)
+		return (0);
 	i = 0;
+	if (str[i] == '+' || str[i] == '-')
+		i++;
+	if (!str[i])
+		return (0);
 	while (str[i])
 	{
-		if (str[i] < 48 || str[i] > 57)
+		if (str[i] < '0' || str[i] > '9')
 			return (0);
 		i++;
 	}
@@ -35,18 +41,21 @@ int	builtin_exit(char **args)
 	ft_putendl_fd("exit", 1);
 	if (args && args[1])
 	{
-		if (!is_numstr((args[1])))
+		if (!is_numstr(args[1]))
 		{
+			ft_putstr_fd("exit: ", 2);
+			ft_putstr_fd(args[1], 2);
+			ft_putendl_fd(": numeric argument required", 2);
 			free_all_garbage();
 			rl_clear_history();
 			exit(2);
 		}
-		exit_code = ft_atoi(args[1]);
 		if (args[2])
 		{
 			ft_putendl_fd("exit: too many arguments", 2);
 			return (1);
 		}
+		exit_code = ft_atoi(args[1]);
 	}
 	free_all_garbage();
 	rl_clear_history();
