0e49f69940761a43f3b930dbfb5b28a10f129cd9
Simon Wied <swied@1-A-23.42heilbronn.de>
2025-09-22 18:28:45 +0200

    FIX(heredoc): Fixed the remove_quotes and check_del_for_quotes so no leaks any more and fully working

--- src/heredoc_utils.c
+++ src/heredoc_utils.c
@@ -6,7 +6,7 @@
 /*   By: swied <swied@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/22 17:39:33 by swied             #+#    #+#             */
-/*   Updated: 2025/09/22 18:08:21 by swied            ###   ########.fr       */
+/*   Updated: 2025/09/22 18:27:15 by swied            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,55 +15,50 @@
 /* removes all quotes in delimiter */
 static void	remove_quotes(char **delimiter, int count)
 {
-	int		i;
-	int		j;
-	char	*tmp;
-	char	*del;
+    int		i;
+    int		j;
+    char	*tmp;
+    char	*del;
 
-	i = 0;
-	j = 0;
-	del = delimiter[0];
-	tmp = malloc(count * sizeof(char *) + 1);
-	if (!tmp)
-		return ;
-	while (del[i])
-	{
-		if (del[i] != '\"' && del[i] != '\'')
-		{
-			tmp[j] = del[i];
-			j++;
-		}
-		i++;
-	}
-	tmp[j] = '\0';
-	gc_free(del);
-	delimiter[0] = ft_strdup(tmp);
-	add_to_gc(delimiter[0]);
-	return ;
+    i = 0;
+    j = 0;
+    del = delimiter[0];
+    tmp = gc_malloc((ft_strlen(del) - count + 1) * sizeof(char));
+    if (!tmp)
+        return ;
+    while (del[i])
+    {
+        if (del[i] != '\"' && del[i] != '\'')
+            tmp[j++] = del[i];
+        i++;
+    }
+    tmp[j] = '\0';
+    gc_free(del);
+    delimiter[0] = tmp;
 }
 
 /* checks if delimiter has quotes and returns the flag */
 int	check_del_for_quotes(char **delimiter)
 {
-	int		i;
-	int		flag;
-	int		count;
-	char	*del;
+    int		i;
+    int		flag;
+    int		count;
+    char	*del;
 
-	i = 0;
-	flag = 0;
-	count = 0;
-	del = delimiter[0];
-	while (del[i])
-	{
-		if (del[i] == '\"' || del[i] == '\'')
-		{
-			flag = 1;
-			count++;
-		}
-		i++;
-	}
-	if (flag)
-		remove_quotes(delimiter, count);
-	return (flag);
+    i = 0;
+    flag = 0;
+    count = 0;
+    del = delimiter[0];
+    while (del[i])
+    {
+        if (del[i] == '\"' || del[i] == '\'')
+        {
+            flag = 1;
+            count++;
+        }
+        i++;
+    }
+    if (flag)
+        remove_quotes(delimiter, count);
+    return (flag);
 }
