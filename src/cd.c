54cec6b7a286d88ad455f665c42bd0659cf346dd
Simon Wied <swied@2-B-7.42heilbronn.de>
2025-09-25 12:31:50 +0200

    FIX(cd): Added check if HOME is not set + fixed segfault with check for no var

--- src/cd.c
+++ src/cd.c
@@ -6,32 +6,20 @@
 /*   By: swied <swied@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/15 02:17:26 by swied             #+#    #+#             */
-/*   Updated: 2025/09/23 00:24:54 by swied            ###   ########.fr       */
+/*   Updated: 2025/09/25 12:29:03 by swied            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/execute.h"
 
-/* change HOME and OLDPWD in env_list | change directory with chdir */
-int	builtin_cd(char **args, t_env_list *env_list)
+static int	builtin_cd_two(char *target_dir, t_env_list *env_list,
+		char *old_pwd)
 {
-	char	*target_dir;
-	char	*old_pwd;
 	char	*new_pwd;
 
-	if (!args[1])
-		target_dir = get_env_value(env_list, "HOME");
-	if (args[2])
-		return (ft_putendl_fd("minishell: cd: too many arguments", 2), 1);
-	else if (ft_strcmp(args[1], "-") == 0)
-		target_dir = get_env_value(env_list, "OLDPWD");
-	else
-		target_dir = args[1];
-	old_pwd = getcwd(NULL, 0);
-	add_to_gc(old_pwd);
 	if (chdir(target_dir))
 	{
-		ft_putstr_fd("minishell: no such file or directory", 2);
+		ft_putstr_fd("minishell: no such file or directory: ", 2);
 		return (ft_putendl_fd(target_dir, 2), 1);
 	}
 	set_env_var(env_list, "OLDPWD", old_pwd);
@@ -40,4 +28,31 @@ int	builtin_cd(char **args, t_env_list *env_list)
 		return (ft_putendl_fd("minishell: cd failed", 2), 2);
 	set_env_var(env_list, "PWD", new_pwd);
 	return (add_to_gc(new_pwd), 0);
+}
+
+/* change HOME and OLDPWD in env_list | change directory with chdir */
+int	builtin_cd(char **args, t_env_list *env_list)
+{
+	char	*target_dir;
+	char	*old_pwd;
+
+	if (!args[1])
+	{
+		target_dir = get_env_value(env_list, "HOME");
+		if (!target_dir)
+			return (ft_putendl_fd("minishell: cd: HOME not set", 2), 1);
+	}
+	else if (args[2])
+		return (ft_putendl_fd("minishell: cd: too many arguments", 2), 1);
+	else if (ft_strcmp(args[1], "-") == 0)
+	{
+		target_dir = get_env_value(env_list, "OLDPWD");
+		if (!target_dir)
+			return (ft_putendl_fd("minishell: cd: OLDPWD not set", 2), 1);
+	}
+	else
+		target_dir = args[1];
+	old_pwd = getcwd(NULL, 0);
+	add_to_gc(old_pwd);
+	return (builtin_cd_two(target_dir, env_list, old_pwd));
 }
