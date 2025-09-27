188e4114b7b9c8c8179628839df41866414ed908
aimonkied <simon.wied2000@gmail.com>
2025-09-28 01:10:12 +0200

    FIX(redirect): set back the stdin and stdout after one cmd builtin

--- src/execute.c
+++ src/execute.c
@@ -6,7 +6,7 @@
 /*   By: swied <swied@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/14 23:04:36 by swied             #+#    #+#             */
-/*   Updated: 2025/09/25 10:36:06 by swied            ###   ########.fr       */
+/*   Updated: 2025/09/28 01:07:21 by swied            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,21 +31,40 @@ int	execute_cmd_or_builtin(t_cmd_node *cmd_node, t_env_list *env_list)
 	return (1);
 }
 
+static int	close_backups(int stdin_backup, int stdout_backup)
+{
+	if (stdin_backup != -1)
+		close(stdin_backup);
+	if (stdout_backup != -1)
+		close(stdout_backup);
+	return (1);
+}
+
 /* if only one cmd and its a builtin, then execute in main process |
 else execute pipes */
 int	execute_loop(t_cmd_list *cmd_list, t_env_list *env_list)
 {
 	int	status;
+	int	stdin_backup;
+	int	stdout_backup;
 
 	if (handle_cmd_list_heredocs(cmd_list, env_list) == -1)
 		return (1);
 	if (cmd_list->size == 1 && cmd_list->head->cmd_type == 1)
 	{
+		stdin_backup = dup(STDIN_FILENO);
+		stdout_backup = dup(STDOUT_FILENO);
+		if (stdin_backup == -1 || stdout_backup == -1)
+			return (close_backups(stdin_backup, stdout_backup), 1);
 		setup_pipes(cmd_list, STDIN_FILENO, 0);
 		if (redirect(cmd_list->head) != 0)
 			status = 1;
 		else
 			status = execute_builtin(cmd_list->head, env_list);
+		dup2(stdin_backup, STDIN_FILENO);
+		dup2(stdout_backup, STDOUT_FILENO);
+		close(stdin_backup);
+		close(stdout_backup);
 	}
 	else
 		status = execute_pipes(cmd_list, env_list);
